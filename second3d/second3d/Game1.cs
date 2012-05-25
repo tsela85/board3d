ed478d30bd85a5a0a6a405dd7c1ab3ab65184c5d
Tom Sela <tsela85@gmail.com>
2012-05-25 21:22:39 +0300

    first commit

---
+++ second3d/second3d/Game1.cs
@@ -0,0 +1,227 @@
+ using System;
+ using System.Collections.Generic;
+ using System.Linq;
+ using Microsoft.Xna.Framework;
+ using Microsoft.Xna.Framework.Audio;
+ using Microsoft.Xna.Framework.Content;
+ using Microsoft.Xna.Framework.GamerServices;
+ using Microsoft.Xna.Framework.Graphics;
+ using Microsoft.Xna.Framework.Input;
+ using Microsoft.Xna.Framework.Media;
+ 
+ namespace second3d
+ {
+     public class Game1 : Microsoft.Xna.Framework.Game
+     {
+         GraphicsDeviceManager graphics;
+         SpriteBatch spriteBatch;
+         GraphicsDevice device;
+         Effect effect;
+         Texture2D texture;
+         private float angle = 0f;
+         PolyTexture board;
+         KeyboardState lstk;
+
+         VertexPositionTexture[] vertices;
+
+         Matrix viewMatrix;
+         Matrix projectionMatrix;
+         Matrix worldMatrix;
+
+         public Game1()
+         {
+             graphics = new GraphicsDeviceManager(this);
+             Content.RootDirectory = "Content";
+         }
+
+         protected override void Initialize()
+         {
+             graphics.PreferredBackBufferWidth = 800;
+             graphics.PreferredBackBufferHeight = 600;
+             graphics.IsFullScreen = false;
+             graphics.ApplyChanges();
+             board = new PolyTexture();
+             this.IsMouseVisible = true;
+             Window.Title = "Lets Fold It!!";
+
+
+             base.Initialize();
+         }
+
+         protected override void LoadContent()
+         {
+             spriteBatch = new SpriteBatch(GraphicsDevice);
+
+             device = graphics.GraphicsDevice;
+
+             effect = Content.Load<Effect>("effects");
+             texture = Content.Load<Texture2D>("images");
+             Vector3[] points = new Vector3[4] {
+                new Vector3(-20f, 0f, 10f),
+                new Vector3(20f, 0f, 10f),
+                new Vector3(20f, 0f, -10f),
+                new Vector3(-20f, 0f, -10f)
+             };
+             Vector2[] texCords = new Vector2[4] {
+                new Vector2(0,0),
+                new Vector2(1,0),
+                new Vector2(1,1),
+                new Vector2(0,1)  
+             };
+             board.Initialize(texture, 4, points, texCords);
+             SetUpCamera();
+
+             SetUpVertices();
+         }
+
+         private void SetUpCamera()
+         {
+             viewMatrix = Matrix.CreateLookAt(new Vector3(0, -50, 0), new Vector3(0, 0, 0), new Vector3(0, 0, 1));
+             projectionMatrix = Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4, device.Viewport.AspectRatio, 0.2f, 500.0f);
+         }
+
+         private void SetUpVertices()
+         {
+             vertices = new VertexPositionTexture[6];
+
+             vertices[0].Position = new Vector3(-10f, 10f, 0f);
+             vertices[0].TextureCoordinate.X = 0;
+             vertices[0].TextureCoordinate.Y = 0;
+
+
+             vertices[1].Position = new Vector3(10f, -10f, 0f);
+             vertices[1].TextureCoordinate.X = 1;
+             vertices[1].TextureCoordinate.Y = 1;
+
+             vertices[2].Position = new Vector3(-10f, -10f, 0f);
+             vertices[2].TextureCoordinate.X = 0;
+             vertices[2].TextureCoordinate.Y = 1;
+
+             vertices[3].Position = new Vector3(10f, -10f, 0f);
+             vertices[3].TextureCoordinate.X = 1;
+             vertices[3].TextureCoordinate.Y = 1;
+
+             vertices[4].Position = new Vector3(-10f, 10f, 0f);
+             vertices[4].TextureCoordinate.X = 0;
+             vertices[4].TextureCoordinate.Y = 0;
+
+             vertices[5].Position = new Vector3(10f, 10f, 0f);
+             vertices[5].TextureCoordinate.X = 1;
+             vertices[5].TextureCoordinate.Y = 0;
+         }
+
+         protected override void UnloadContent()
+         {
+         }
+
+         protected override void Update(GameTime gameTime)
+         {
+             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                 this.Exit();
+             KeyboardState keyState = Keyboard.GetState();
+             MouseState ms = Mouse.GetState();
+             Vector3 mouse = GetPickedPosition(new Vector2((float)ms.X, (float)ms.Y));
+
+             if (ms.LeftButton == ButtonState.Pressed)
+             {
+                 board.collideWithEdge(mouse);
+
+             }
+             if (lstk.IsKeyDown(Keys.Left) && (keyState.IsKeyUp(Keys.Left)))
+             {
+                 //angle = MathHelper.Pi;
+                 //direction = 1;
+                 //angle += 0.001f;
+                 angle = 0.1f;
+                 Matrix rotateMatrix = Matrix.CreateFromAxisAngle(vertices[1].Position - vertices[0].Position, angle);
+                 vertices[2].Position = Vector3.Transform(vertices[2].Position, rotateMatrix);
+
+
+             }
+             if (lstk.IsKeyDown(Keys.Right) && (keyState.IsKeyUp(Keys.Right)))
+             {
+                 //angle = 0; ;
+                 //direction = 1;
+                 angle -= 0.001f;
+             }
+
+             // direction = ((angle < MathHelper.Pi) && (angle > 0) ? direction : (direction+1)%2);
+             // angle += ((direction == 0) ?  -0.00001f : 0.00001f);             
+
+             lstk = keyState;
+
+             base.Update(gameTime);
+         }
+
+         protected override void Draw(GameTime gameTime)
+         {
+             device.Clear(ClearOptions.Target | ClearOptions.DepthBuffer, Color.DarkSlateBlue, 1.0f, 0);
+             RasterizerState rs = new RasterizerState();
+             rs.CullMode = CullMode.None;
+             rs.FillMode = FillMode.WireFrame;
+             device.RasterizerState = rs;
+
+
+             worldMatrix = Matrix.Identity;
+             effect.CurrentTechnique = effect.Techniques["TexturedNoShading"];
+             effect.Parameters["xWorld"].SetValue(worldMatrix);
+             effect.Parameters["xView"].SetValue(viewMatrix);
+             effect.Parameters["xProjection"].SetValue(projectionMatrix);
+             effect.Parameters["xTexture"].SetValue(texture);
+
+             foreach (EffectPass pass in effect.CurrentTechnique.Passes)
+             {
+                 pass.Apply();
+
+                 // device.DrawUserPrimitives(PrimitiveType.TriangleList, vertices,0, 2, VertexPositionTexture.VertexDeclaration);
+                 // device.DrawUserPrimitives(PrimitiveType.TriangleList, board.Vertices, 0, 1, VertexPositionTexture.VertexDeclaration);
+
+
+                 board.Draw(ref device);
+             }
+
+             base.Draw(gameTime);
+         }
+
+         //----------------------------------------------------------------
+         // GetPickedPosition() - gets 3D position of mouse pointer
+         //                     - always on the the Y = 0 plane     
+         //----------------------------------------------------------------
+         public Vector3 GetPickedPosition(Vector2 mousePosition)
+         {
+
+             // create 2 positions in screenspace using the cursor position. 0 is as
+             // close as possible to the camera, 10 is as far away as possible
+             Vector3 nearSource = new Vector3(mousePosition, 0f);
+             Vector3 farSource = new Vector3(mousePosition, 1f);
+
+             // find the two screen space positions in world space
+             Vector3 nearPoint = GraphicsDevice.Viewport.Unproject(nearSource, projectionMatrix,viewMatrix,worldMatrix);
+
+             Vector3 farPoint = GraphicsDevice.Viewport.Unproject(farSource,
+                                 projectionMatrix, viewMatrix, worldMatrix);
+
+             // compute normalized direction vector from nearPoint to farPoint
+             Vector3 direction = farPoint - nearPoint;
+             direction.Normalize();
+
+             // create a ray using nearPoint as the source
+             Ray r = new Ray(nearPoint, direction);
+
+             // calculate the ray-plane intersection point
+             Vector3 n = new Vector3(0f, 1f, 0f);
+             Plane p = new Plane(n, 0f);
+
+             // calculate distance of intersection point from r.origin
+             float denominator = Vector3.Dot(p.Normal, r.Direction);
+             float numerator = Vector3.Dot(p.Normal, r.Position) + p.D;
+             float t = -(numerator / denominator);
+
+             // calculate the picked position on the y = 0 plane
+             Vector3 pickedPosition = nearPoint + direction * t;
+
+
+             return pickedPosition;
+         }
+     }
+ }
---
+++ second3d/second3d/PolyTexture.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace second3d
+{
+    class PolyTexture
+    {
+        private VertexPositionNormalTexture[] vertices;
+        private short[] indices;
+        int vertNum;      
+        private Texture2D texture;
+        private Vector2 pointOnEdge;
+        VertexPositionTexture[] ver;
+
+#region properties
+
+        public VertexPositionNormalTexture[] Vertices
+        {
+            get{ return vertices;}
+            set {vertices = value;}
+        }
+
+        public short[] Indices
+        {
+            get{ return indices;}
+            set {indices = value;}
+        }
+ 
+#endregion
+        
+        public void Initialize(Texture2D tex, int vNum, Vector3[] points,Vector2[] texCords)
+        {
+            pointOnEdge = Vector2.Zero;
+            int iCount = (vNum - 3) * 3 + 3;
+            vertNum = vNum;
+            texture = tex;
+            vertices = new VertexPositionNormalTexture[vNum];
+
+            indices = new short[iCount];
+            for (int i = 0; i < vNum; i++)
+            {
+                vertices[i].Position = points[i];
+                vertices[i].TextureCoordinate = texCords[i];
+                vertices[i].Normal = Vector3.Up;
+            }
+            //create the indices. every 2 consecutive points makes an outer edge (x,y,z) -> xy , yz (not xz)
+            short j = 0;
+            for (short i = 0; i < iCount; i+=3)
+            {
+                indices[i] = j;
+                indices[i+1] = (short)(j + 1);
+                indices[i + 2] = (short)((j + 2) % vNum);
+                j += 2;
+            }
+            ver = new VertexPositionTexture[3];
+        }
+
+        public void Draw(ref GraphicsDevice device)
+        {
+            device.DrawUserIndexedPrimitives(PrimitiveType.TriangleList, vertices, 0,
+                vertices.Length, indices, 0, indices.Length / 3, VertexPositionNormalTexture.VertexDeclaration);
+            
+            
+            device.DrawUserPrimitives(PrimitiveType.TriangleList, ver, 0, 1, VertexPositionNormalTexture.VertexDeclaration);
+        }
+
+ #region Collision
+        public bool collideWithEdge(ref GraphicsDevice device,Matrix project,Matrix view, Matrix world, Vector2 point)
+        {
+            Vector3 one, two;
+            one = device.Viewport.Project(vertices[0].Position, project, view, world);            
+            for (int i = 0; i < vertNum - 1; i++)
+            {
+                two = device.Viewport.Project(vertices[i+1].Position, project, view, world);
+                if (LineIntersectPoint(one, two, point, 5))
+                {
+                    one = device.Viewport.Unproject(new Vector3(pointOnEdge,0f), project, view, world);   
+                    ver[0].Position = new Vector3(pointOnEdge.X - 0.5f, pointOnEdge.Y - 0.5f, 0f);
+                    ver[0].TextureCoordinate = Vector2.Zero;
+                    ver[1].Position = new Vector3(pointOnEdge.X, pointOnEdge.Y + 0.5f, 0f);
+                    ver[1].TextureCoordinate = Vector2.Zero;
+                    ver[2].Position = new Vector3(pointOnEdge.X + 0.5f, pointOnEdge.Y - 0.5f, 0f);
+                    ver[2].TextureCoordinate = Vector2.Zero;
+                    return true;
+                }
+                one = two;
+            }
+            return false;
+        }
+
+
+        private Vector2 DistanceLineSegmentToPoint(Vector2 A, Vector2 B, Vector2 p, float dist)
+        {
+            //get the normalized line segment vector
+            Vector2 v = B - A;
+            v.Normalize();
+
+            //determine the point on the line segment nearest to the point p
+            float distanceAlongLine = Vector2.Dot(p, v) - Vector2.Dot(A, v);
+            Vector2 nearestPoint;
+            if (distanceAlongLine < 0)
+            {
+                //closest point is A
+                nearestPoint = A;
+            }
+            else if (distanceAlongLine > Vector2.Distance(A, B))
+            {
+                //closest point is B
+                nearestPoint = B;
+            }
+            else
+            {
+                //closest point is between A and B... A + d  * ( ||B-A|| )
+                nearestPoint = A + distanceAlongLine * v;
+            }
+
+            //Calculate the distance between the two points
+            float actualDistance = Vector2.Distance(nearestPoint, p);
+            if (actualDistance < dist)
+                return nearestPoint;
+            return Vector2.Zero;
+        }
+
+        private bool LineIntersectPoint(Vector3 v1, Vector3 v2, Vector2 p, float dist)
+        {
+            Vector2 A = new Vector2(v1.X, v1.Y);
+            Vector2 B = new Vector2(v2.X, v2.Y);
+            Vector2 nearestPoint = DistanceLineSegmentToPoint(A, B, p,dist);
+            if (nearestPoint != Vector2.Zero)
+            {
+                pointOnEdge = nearestPoint;
+                return true;
+            }
+            else
+                return false;
+        }
+#endregion
+
+    }
+}
